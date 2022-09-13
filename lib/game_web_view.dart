7767aa0afeb9fe69a6606c6b2bf9ab0b70b1d1c9
Varsha Jangid <varshajangid1999@gmail.com>
2022-09-13 02:18:35 -1000

    create game web view and navigate on it

---
+++ lib/game_web_view.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+
+import 'package:webview_flutter/webview_flutter.dart';
+
+class GameWebView extends StatefulWidget {
+  final String gameURL;
+
+  const GameWebView({required this.gameURL});
+
+  @override
+  State createState() => _GameWebViewStat();
+}
+
+class _GameWebViewStat extends State<GameWebView> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: WebView(
+        initialUrl: widget.gameURL,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+        onProgress: (int progress) {
+          print('WebView is loading (progress : $progress%)');
+        },
+        javascriptChannels: <JavascriptChannel>{
+          _toasterJavascriptChannel(context),
+        },
+        navigationDelegate: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            print('blocking navigation to $request}');
+            return NavigationDecision.prevent;
+          }
+          print('allowing navigation to $request');
+          return NavigationDecision.navigate;
+        },
+        onPageStarted: (String url) {
+          print('Page started loading: $url');
+        },
+        onPageFinished: (String url) {
+          print('Page finished loading: $url');
+        },
+        gestureNavigationEnabled: true,
+        backgroundColor: const Color(0x00000000),
+      ),
+    );
+  }
+}
